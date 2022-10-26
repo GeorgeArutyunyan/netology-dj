21f3bc8fafb6c47cd5b87501130d96f10ac8f065
George Arutyunyan <arutunyan.george@gmail.com>
2022-10-26 20:25:13 +0300

    m2m

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                counter += 1
+        if counter == 0:
+            raise ValidationError('Отсутсвует тег основного раздела')
+        elif counter >= 2:
+            raise ValidationError('Может быть только один основной тег')
+
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class Tag(admin.ModelAdmin):
     pass
