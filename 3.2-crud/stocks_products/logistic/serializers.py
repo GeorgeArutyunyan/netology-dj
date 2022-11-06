9111d13f2ea0f653207862f07874be2fbbc90b35
George Arutyunyan <arutunyan.george@gmail.com>
2022-11-06 14:17:36 +0300

    hw5

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,25 @@
 from rest_framework import serializers
+from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
+
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
-
+    class Meta:
+        model = StockProduct
+        fields = ['stock', 'product', 'quantity', 'price']
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['adress', 'products']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -22,11 +27,8 @@ class StockSerializer(serializers.ModelSerializer):
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for item in positions:
+            StockProduct.objects.create(stock=stock, **item)
         return stock
 
     def update(self, instance, validated_data):
@@ -36,8 +38,8 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for item in positions:
+            StockProduct.ojbects.update_or_create(
+                **item
+            )
         return stock
